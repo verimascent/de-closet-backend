a5c7fb4239e2564e4a3edc8bcf47cb9e67016249
relaxyg <relaxyg4@gmail.com>
2021-11-29 18:00:19 +0800

    rails db:create db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,37 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_11_29_080508) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "giveaways", force: :cascade do |t|
+    t.text "status"
+    t.bigint "user_id", null: false
+    t.bigint "item_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["item_id"], name: "index_giveaways_on_item_id"
+    t.index ["user_id"], name: "index_giveaways_on_user_id"
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.boolean "is_giveaway"
+    t.text "item_type"
+    t.text "remark"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "giveaways", "items"
+  add_foreign_key "giveaways", "users"
+  add_foreign_key "items", "users"
 end
