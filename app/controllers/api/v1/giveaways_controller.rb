f92b246ee36e433821a1889f499a7d71f47ad012
verimascent <verimascent@gmail.com>
2021-12-09 12:50:15 +0800

    giveaways controller - change instance variable to local variable

--- app/controllers/api/v1/giveaways_controller.rb
+++ app/controllers/api/v1/giveaways_controller.rb
@@ -2,18 +2,18 @@ class Api::V1::GiveawaysController < Api::V1::BaseController
 
   # The giveaway shall be created by the user who claims that item
   def create
-    @item = Item.find(params[:item_id])
-    @giveaway = Giveaway.new
-    @giveaway.user = current_user
-    @giveaway.item = @item
+    item = Item.find(params[:item_id])
+    giveaway = Giveaway.new
+    giveaway.user = current_user
+    giveaway.item = item
 
-    unless @giveaway.save
-      puts "THIS IS ERROR MES, #{@giveaway.errors.full_messages}"
+    unless giveaway.save
+      puts "THIS IS ERROR MES, #{giveaway.errors.full_messages}"
     end
 
     if current_user.update(update_user_params)
       render json: {
-        giveaway: @giveaway
+        giveaway: giveaway
       }
     else
       puts "THIS IS ERROR MES, #{current_user.errors.full_messages}"
@@ -22,16 +22,16 @@ class Api::V1::GiveawaysController < Api::V1::BaseController
 
   # This update action can only be invoked by the owner!
   def update
-    @giveaway = Giveaway.find(giveaway_params[:id])
+    giveaway = Giveaway.find(giveaway_params[:id])
 
-    unless current_user == @giveaway.item.user
+    unless current_user == giveaway.item.user
       render json: { message: 'You are not permitted to change the status of this giveaway.' }
       return
     end
 
-    @item = @giveaway.item
-    @giveaways_all = @item.giveaways
-    @giveaways_all.each do |ga|
+    item = giveaway.item
+    giveaways_all = item.giveaways
+    giveaways_all.each do |ga|
       if ga.user.id == giveaway_params[:user_id]
         ga.update(status: 'accepted')
       else
@@ -39,10 +39,10 @@ class Api::V1::GiveawaysController < Api::V1::BaseController
       end
     end
 
-    @item.user = User.find(giveaway_params[:user_id])
+    item.user = User.find(giveaway_params[:user_id])
 
     render json: {
-      item: @item.all_info
+      item: item.all_info
     }
   end
 
